49f119f8f2940b7d7575933de99767bebd9a3617
bhclira <brenohencastro@gmail.com>
2025-01-27 11:23:55 -0300

    feat: adiciona estilo ao conversor de moedas e melhora a interface do usuário

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/* function Dolar() {
-    var valor = document.getElementById("valor").value;
-    var valorConvertido = valor * 5.25; // Example conversion rate
-    document.getElementById("valorConvertido").innerText = "Valor em Dólar: $" + valorConvertido.toFixed(2);
-}
-
-function Euro() {
-    var valor = document.getElementById("valor").value;
-    var valorConvertido = valor * 6.20; // Example conversion rate
-    document.getElementById("valorConvertido").innerText = "Valor em Euro: €" + valorConvertido.toFixed(2);
-}
-
-function Bitcoin() {
-    var valor = document.getElementById("valor").value;
-    var valorConvertido = valor * 0.000008; // Example conversion rate
-    document.getElementById("valorConvertido").innerText = "Valor em Bitcoin: ₿" + valorConvertido.toFixed(8);
-} */
-
 function convertToDolar() {
     let valor = document.getElementById("valor").value;
     let valorConvertido = valor * 5.25; // peguei o valor obtido do usuário e multipliquei pela cotação do dolar atural
@@ -37,5 +19,5 @@ function convertToBitcoin() {
 
 function limparCampos() {
     document.getElementById("valor").value = "";
-    document.getElementById("valorConvertido").innerText = "Valor Convertido:";
+    document.getElementById("valorConvertido").innerText = "valor convertido:";
 }
